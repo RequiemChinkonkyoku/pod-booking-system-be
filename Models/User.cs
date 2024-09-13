d50002b681dc5fa94935160d54bbc81fe58fecf4
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-09-13 19:50:54 +0700

    feat: initialized 3-layer-arch and added initial migration for User table

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? PasswordHash { get; set; }
+        public int? Status { get; set; }
+        public int? MembershipId { get; set; }
+    }
+}
---
+++ Repositories/PodBookingSystemDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class PodBookingSystemDbContext : DbContext
+    {
+        public PodBookingSystemDbContext()
+        {
+
+        }
+        public PodBookingSystemDbContext(DbContextOptions<PodBookingSystemDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("connectionstrings.json", true, true)
+                    .Build();
+                var connectionString = configuration.GetConnectionString("PodBookingSystemDB");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        //DbSet for custom entities
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
