37fcf95715dd7e045920f91f507d0491027e17be
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-09-18 02:35:44 +0700

    feat: added packages and implemented impl/interface for repo

---
+++ Repositories/Implement/RepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Implement
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        PodBookingSystemDbContext _context;
+        DbSet<T> _dbSet;
+
+        public RepositoryBase()
+        {
+            _context = new PodBookingSystemDbContext();
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public void Add(T item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+        }
+
+        public async Task AddAsync(T item)
+        {
+            await _dbSet.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(T item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+        public async Task DeleteAsync(T item)
+        {
+            _dbSet.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T item)
+        {
+            _dbSet.Update(item);
+            _context.SaveChanges();
+        }
+
+        public async Task UpdateAsync(T item)
+        {
+            _dbSet.Update(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> FindByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+    }
+}
---
+++ Repositories/Interface/IRepositoryBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interface
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+
+        void Add(T item);
+
+        public Task AddAsync(T item);
+
+        void Update(T item);
+
+        public Task UpdateAsync(T item);
+
+        void Delete(T item);
+
+        Task<T> FindByIdAsync(int id);
+    }
+}
