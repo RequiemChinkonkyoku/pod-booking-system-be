1eb8ffc86f4cd5c1cf8fe0a069a9e7cbb71b4239
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-09-20 07:50:34 +0700

    fix: updated table selectedProduct

--- Models/SelectedProduct.cs
+++ Models/SelectedProduct.cs
@@ -14,6 +14,7 @@ namespace Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int Quantity { get; set; }
+        public int ProductPrice { get; set; }
 
         public int ProductId { get; set; }
         public Product Product { get; set; }
---
+++ Repositories/Migrations/20240920004732_V1_UpdateSelectedProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1_UpdateSelectedProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductPrice",
+                table: "SelectedProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductPrice",
+                table: "SelectedProducts");
+        }
+    }
+}
