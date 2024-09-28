362f62757f7c249b544dd17ba68e366201c30d4f
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-09-29 02:38:49 +0700

    feat: seed data for user table

--- Repositories/Migrations/20240928193744_V1_SeedDataUser.Designer.cs
+++ Repositories/Migrations/20240928193744_V1_SeedDataUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repositories;
 
@@ -11,9 +12,11 @@ using Repositories;
 namespace Repositories.Migrations
 {
     [DbContext(typeof(PodBookingSystemDbContext))]
-    partial class PodBookingSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240928193744_V1_SeedDataUser")]
+    partial class V1_SeedDataUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Repositories/Migrations/20240928193744_V1_SeedDataUser.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1_SeedDataUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "N/A", "N/A" });
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "No bonuses", "Regular" });
+
+            migrationBuilder.InsertData(
+                table: "Memberships",
+                columns: new[] { "Id", "Description", "Name", "Status" },
+                values: new object[] { 3, "VIPPRO", "VIP", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "MembershipId", "Name", "Password", "PasswordHash", "RoleId", "Status" },
+                values: new object[,]
+                {
+                    { 1, "customer@gmail.com", 2, "CUSTOMER", "Customer@1234", "$2a$11$9LxHPbEnXmZvbjWIX5OIz.ozzatDgE.EOCWl0MqRAbar/frUinybu", 1, 1 },
+                    { 2, "staff@gmail.com", 1, "STAFF", "Staff@1234", "$2a$11$KMKRtWyFCKmShT1nz8DkhOHVOFme2MGjZWp9Wl9eOQpxtPv1pDzuK", 2, 1 },
+                    { 3, "manager@gmail.com", 1, "MANAGER", "Manager@1234", "$2a$11$RunWU.yYiPBD4HQ/R7uNQufrax0oFvvxunX4T1mwdD/dWIXHhiPXa", 3, 1 },
+                    { 4, "admin@gmail.com", 1, "ADMIN", "Admin@1234", "$2a$11$ZRknrBy3d7rEBc.ji3F2WeUBpT4cgt87BPQ7QSN5Aa6pLFfKbRq9q", 4, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "No bonuses", "Regular" });
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "VIPPRO", "VIP" });
+        }
+    }
+}
