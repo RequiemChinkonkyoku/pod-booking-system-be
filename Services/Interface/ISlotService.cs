4aa4e8a420f1a0eb3c2b960dad79c0cb3e40c24e
TienTran23 <trannhuttienvn23@gmail.com>
2024-10-15 13:07:02 +0700

    feat: add get slot By PodType

--- Services/Interface/ISlotService.cs
+++ Services/Interface/ISlotService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Services.Interface
 {
     public interface ISlotService
     {
+        public Task<List<Slot>> GetSlotBySlotTypeAsync(int id);
+
     }
 }
