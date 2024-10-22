504027e1396c2d7e6b4812aed7f42c55b0ea179f
TNKLBAH <putinkhanh112@gmail.com>
2024-10-22 10:23:20 +0700

    feature: add getAllMethod

---
+++ PodBookingSystem.API/Controllers/MethodController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interface;
+
+namespace PodBookingSystem.API.Controllers
+{
+    public class MethodController : Controller
+    {
+        private readonly IMethodService _methodService;
+
+        public MethodController(IMethodService methodService)
+        {
+            _methodService = methodService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMethod()
+        {
+            var response = await _methodService.GetAllMethod();
+
+            return Ok(response);
+        }
+
+    }
+}
