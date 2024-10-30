3fef5ff40b96befe968a2b5bc5b6618265dee27b
TNKLBAH <putinkhanh112@gmail.com>
2024-10-31 00:17:22 +0700

    feature: add getTransaction

--- Services/Interface/ITransactionService.cs
+++ Services/Interface/ITransactionService.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
+using Transaction = Models.Transaction;
 
 namespace Services.Interface
 {
     public interface ITransactionService
     {
-        Task<Models.Transaction> CreateTransaction(int id, bool success);
+        public Task<Transaction> CreateTransaction(int id, bool success);
+        public Task<List<Transaction>> GetAllTransaction();
+        public Task<TransactionServiceResponse> GetSuccessTransactionByBookingId(int id);
+        public Task<TransactionServiceResponse> GetTransactionById(int id);
     }
 }
