9f59f5344497e8b7a8d71ffb64b52a88a727f33c
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-11-11 22:50:53 +0700

    feat: added forgot password funcs

--- Services/Interface/IAuthService.cs
+++ Services/Interface/IAuthService.cs
@@ -13,5 +13,7 @@ namespace Services.Interface
         Task<string> Login (string email, string password);
         Task<User> RegisterCustomer(CreateUserDto dto);
         Task<bool> VerifyOtpAsync(string email, string otpCode);
+        Task<string> ForgotPasswordAsync(string email);
+        Task<bool> ResetPasswordAsync(string email, string otpCode, string newPassword);
     }
 }
